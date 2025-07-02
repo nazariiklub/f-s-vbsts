788f7109df3ad9c4c01a4aeb9faf53985ba30c2b
Nazarii Klub <155299604+nazariiklub@users.noreply.github.com>
2025-07-03 00:09:50 +0300

    Add new menu

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let to_cards = document.getElementById("btn-scroll");
 let cards = document.getElementById("robyty");
+let to_media = document.getElementById("btn-scroll2");
+let media = document.getElementById("media");
 
 to_cards.addEventListener("click", function() {
   cards.scrollIntoView({ behavior: "smooth" });
+});
+
+to_media.addEventListener("click", function() {
+  media.scrollIntoView({ behavior: "smooth" });
 });
