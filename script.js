5fd45ef9c6b80de2c4f51615e2d38a90e9f2d2e6
Nazarii Klub <155299604+nazariiklub@users.noreply.github.com>
2025-08-27 14:56:45 +0300

    upd

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ let to_cards = document.getElementById("btn-scroll");
 let cards = document.getElementById("robyty");
 let to_media = document.getElementById("btn-scroll2");
 let media = document.getElementById("media");
+let CopyText = document.getElementById("copytext")
+let doorPage = document.querySelector(".door-webp");
+let doorCard = document.querySelector(".card_div"); 
+let backBtn = document.querySelector(".back");
+let mainSection = document.querySelector("main");
+let cardDivs = document.querySelectorAll(".card_div");
 
 to_cards.addEventListener("click", function() {
   cards.scrollIntoView({ behavior: "smooth" });
@@ -10,3 +16,42 @@ to_cards.addEventListener("click", function() {
 to_media.addEventListener("click", function() {
   media.scrollIntoView({ behavior: "smooth" });
 });
+
+function copyText() {
+  const text = document.getElementById("CopyText").innerText;
+
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      alert("Текст скопійовано! ✅");
+    })
+    .catch(err => {
+      console.error("Помилка копіювання: ", err);
+    });
+}
+
+
+cardDivs.forEach(card => {
+  card.addEventListener("click", function() {
+    mainSection.style.display = "none";
+
+    let targetClass = card.getAttribute("data-target");
+    let targetSection = document.querySelector(`.${targetClass}`);
+
+    let allSections = document.querySelectorAll(".doors-webp, .stears-webp, .benches-webp, .tables-webp, .Beds-webp, .wardrobe-webp");
+    allSections.forEach(sec => sec.style.display = "none");
+
+    targetSection.style.display = "block";
+
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+});
+
+let backBtns = document.querySelectorAll(".back");
+backBtns.forEach(btn => {
+  btn.addEventListener("click", function() {
+    btn.closest("div[class$='-webp']").style.display = "none";
+    mainSection.style.display = "block";
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+});
+
